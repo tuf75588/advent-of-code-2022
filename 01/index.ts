290442551c3c1ec7e3633f743ac0595eefb9bada
Andrew Davis <atd285@gmail.com>
2022-12-03 14:06:58 -0500

    more elegant solution day one

--- 01/index.ts
+++ 01/index.ts
@@ -1,17 +1,14 @@
 // @ts-ignore
-const text = await Deno.readTextFile('./input.txt');
-const numbers = text.split('\n\n').map((x) => x.split('\n').map(Number));
+const text = await Deno.readTextFile("./input.txt");
+const numbers = text.split("\n\n").map((x) => x.split("\n").map(Number));
 let totals: number[] = [];
 for (const [index, _] of numbers.entries()) {
   const sums = numbers[index].reduce((a, b) => a + b);
   totals.push(sums);
 }
-
 const topAmount = Math.max(...totals);
-const secondHighest = Math.max(
-  ...totals.filter((element) => element !== topAmount)
-);
-const thirdHighest = Math.max(
-  ...totals.filter((counts) => counts !== secondHighest && counts !== topAmount)
-);
-console.log(topAmount + secondHighest + thirdHighest); // 209481
+const topThree = totals.sort((a, b) => {
+  return b - a;
+}).slice(0,3).reduce((a,b) => a + b);
+
+console.log(topThree)
