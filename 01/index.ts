f07511118653584a470e60b68aee74a43cba3055
Andrew Davis <atd285@gmail.com>
2022-12-05 15:51:30 -0500

    formatting

--- 01/index.ts
+++ 01/index.ts
@@ -1,12 +1,12 @@
-// @ts-ignore
+
 const text = await Deno.readTextFile("./input.txt");
 const numbers = text.split("\n\n").map((x) => x.split("\n").map(Number));
-let totals: number[] = [];
+const totals: number[] = [];
 for (const [index, _] of numbers.entries()) {
   const sums = numbers[index].reduce((a, b) => a + b);
   totals.push(sums);
 }
-const topAmount = Math.max(...totals);
+const _topAmount = Math.max(...totals);
 const topThree = totals
   .sort((a, b) => {
     return b - a;
