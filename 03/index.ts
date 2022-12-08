839806e2c71df192eebc39bec5b9cd35e79f06b6
Andrew Davis <atd285@gmail.com>
2022-12-08 15:48:33 -0500

    helper functions day 3 part 1

--- 03/index.ts
+++ 03/index.ts
@@ -1,9 +1,32 @@
 // @ts-ignore
 const input = await Deno.readTextFile('./input.txt');
+const rucksacks = input.split('\n');
+
+const sampleInput = `vJrwpWtwJgWrhcsFMMfFFhFp`;
 
 // split them all in two?
-const types = input.split('\n').map((element) => {
-  const firstPart = element.slice(0, element.length / 2);
-  const secondPart = element.slice(element.length / 2);
-  console.log(firstPart.split('').includes('a'))
-});
+// const types = sampleInput.split('\n').map(splitStringInTwo)
+
+function findCommonItems(rucksack) {
+  const halfIndex = rucksack.length / 2;
+  const [firstHalf, secondHalf] = [
+    rucksack.slice(0, halfIndex),
+    rucksack.slice(halfIndex),
+  ];
+  const firstHalfSet = new Set(firstHalf);
+  const commonItem = [...secondHalf].find((item) => firstHalfSet.has(item));
+  return commonItem;
+
+}
+
+// assign each common character a priority code
+function getItemPriorityCode(item: string) {
+  return item.charCodeAt(0) >= 65 ? item.charCodeAt(0) - 96 : item.charCodeAt(0) - 38;
+}
+
+
+
+// find which characters of the second half are also in the first half
+console.log(getItemPriorityCode(findCommonItems(sampleInput)))
+
+
