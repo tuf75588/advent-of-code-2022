0683f9d326a89afb72a14ae48c5d3ffcac0e6f6b
Andrew Davis <atd285@gmail.com>
2022-12-16 14:48:50 -0500

    pain

--- 07/index.ts
+++ 07/index.ts
@@ -45,10 +45,41 @@ function modifyTree(tree, newKey, newVal, destination, path = []) {
 
 // split up sample input
 
-const lines = sampleInput.split('\n');
+const lines = sample.split('\n');
 const fileTree = lines.reduce(
   ({ tree, location }, line) => {
-    return tree;
+    const doNothing = () => ({ tree, location });
+    const closeDirectory = () => ({ tree, location: location.slice(0, -1) });
+    const goToHomeDirectory = () => ({ tree, location: [] });
+    const openDirectory = (line) => {
+      const dir = line.match(/\$ cd (\w+)/)[1];
+      return { tree, location: [...location, dir] };
+    };
+    const createDirectory = (line) => {
+      const dir = line.match(/dir (\w+)/)[1];
+      const nextTree = modifyTree(tree, dir, {}, location);
+      return { tree, nextTree, location };
+    };
+
+    const createFile = (line) => {
+      const [size, file] = line.match(/(\d+) (.+)/).slice(1);
+      const nextTree = modifyTree(tree, file, Number(size), location);
+      return { tree: nextTree, location };
+    };
+
+    const commandMap = [
+      { expression: /\$ ls/, function: doNothing },
+      { expression: /\$ cd \.\./, function: closeDirectory },
+      { expression: /\$ cd \//, function: goToHomeDirectory },
+      { expression: /\$ cd \w+/, function: openDirectory },
+      { expression: /dir \w+/, function: createDirectory },
+      { expression: /\d+ .+/, function: createFile },
+    ];
+
+    const command = commandMap.find(({expression}) => expression.test(line)).function;
+    return command(line);
   },
   { tree: {}, location: [] }
-);
+).tree;
+
+console.log(fileTree);
